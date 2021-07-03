4a36b2db8f7be77a7a40d3fba6231fdbc6a89d2c
Vlad_Van <v.o.vanyushin@gmail.com>
2021-07-03 16:12:17 +0300

    New method addListOfClient added

--- CRUD/src/main/java/com/example/crud/controller/Controller.java
+++ CRUD/src/main/java/com/example/crud/controller/Controller.java
@@ -7,6 +7,13 @@ import com.example.crud.repo.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @RestController
@@ -25,10 +32,33 @@ public class Controller {
         return converter.entityToDto(clt);
     }
 
+    @PostMapping("/saveall")
+    public List<ClientDto> addListOfClients(@RequestBody List<ClientDto> dto){
+        Iterable<Client> inpt = converter.dtoToEntity(dto);
+        inpt = clientRepository.saveAll(inpt);
+        return converter.entityToDto((List<Client>) inpt);
+    }
+
     @GetMapping("/data")
     public List<ClientDto> getClient(){
         Iterable<Client> findall = clientRepository.findAll();
         return converter.entityToDto((List<Client>)findall);
+    }
+
+    @GetMapping("/dataName")
+    public List<ClientDto> getClientName(){
+//        Iterable<Client> findall = clientRepository.findAll();
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery <Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+        Root<Client> clientRoot = criteriaQuery.from(Client.class);
+        criteriaQuery.select(clientRoot.get("id").get("firstName").get("lastName").get("email"));
+        CriteriaQuery<Client> select = criteriaQuery.select(clientRoot);
+        TypedQuery<Client> query = em.createQuery(select);
+
+        return converter.entityToDto((List<Client>) query);
     }
 
     @GetMapping("find/{id}")
