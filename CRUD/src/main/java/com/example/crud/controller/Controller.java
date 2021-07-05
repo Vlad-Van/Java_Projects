a6ea140dbb3a7772e457a48d4d812c8392875b3c
Vlad_Van <v.o.vanyushin@gmail.com>
2021-07-05 12:36:19 +0300

    Pageable output added

--- CRUD/src/main/java/com/example/crud/controller/Controller.java
+++ CRUD/src/main/java/com/example/crud/controller/Controller.java
@@ -4,17 +4,20 @@ import com.example.crud.converter.Converter;
 import com.example.crud.client.Client;
 import com.example.crud.dto.ClientDto;
 import com.example.crud.repo.ClientRepository;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.function.Function;
 
 @RestController
 public class Controller {
@@ -24,6 +27,9 @@ public class Controller {
 
     @Autowired
     Converter converter;
+
+    @Autowired
+    EntityManager em;
 
     @PostMapping("/save")
     public ClientDto addClient(@RequestBody ClientDto dto) {
@@ -39,26 +45,33 @@ public class Controller {
         return converter.entityToDto((List<Client>) inpt);
     }
 
-    @GetMapping("/data")
-    public List<ClientDto> getClient(){
+    @GetMapping("/alldata")
+    public List<ClientDto> getClientData(){
         Iterable<Client> findall = clientRepository.findAll();
-        return converter.entityToDto((List<Client>)findall);
+        return converter.entityToDto((List<Client>) findall);
+    }
+
+    @GetMapping("/data/{pageNumber}")
+    public Page<ClientDto> getClientPageOne(@PathVariable Integer pageNumber){
+        Pageable pageable = PageRequest.of((pageNumber-1),20);
+        Page<Client> clt = clientRepository.findAll(pageable);
+        return clt.map(new Function<Client, ClientDto>() {
+            @Override
+            public ClientDto apply(Client client) {
+                return new ModelMapper().map(client, ClientDto.class);
+            }
+        });
     }
 
     @GetMapping("/dataName")
     public List<ClientDto> getClientName(){
-//        Iterable<Client> findall = clientRepository.findAll();
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
-        EntityManager em = emf.createEntityManager();
         CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery <Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
         Root<Client> clientRoot = criteriaQuery.from(Client.class);
-        criteriaQuery.select(clientRoot.get("id").get("firstName").get("lastName").get("email"));
+        criteriaQuery.select(clientRoot).where(criteriaBuilder.like(clientRoot.get("firstName"), "%A%"));
         CriteriaQuery<Client> select = criteriaQuery.select(clientRoot);
         TypedQuery<Client> query = em.createQuery(select);
-
-        return converter.entityToDto((List<Client>) query);
+        return converter.entityToDto(query.getResultList());
     }
 
     @GetMapping("find/{id}")
--- CRUD/src/main/java/com/example/crud/repo/ClientRepository.java
+++ CRUD/src/main/java/com/example/crud/repo/ClientRepository.java
@@ -2,7 +2,8 @@ package com.example.crud.repo;
 
 import com.example.crud.client.Client;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ClientRepository extends CrudRepository<Client, Integer>{
+public interface ClientRepository extends PagingAndSortingRepository<Client,Integer>{
     Client findClientById(Integer id);
 }
