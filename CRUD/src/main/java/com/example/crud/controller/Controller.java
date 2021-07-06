6f68c125ee6166ef5d06b550e7530e57ae3c604a
Vlad-Van <v.o.vanyushin@gmail.com>
2021-07-06 19:54:37 +0300

    Update

--- CRUD/src/main/java/com/example/crud/controller/Controller.java
+++ CRUD/src/main/java/com/example/crud/controller/Controller.java
@@ -4,8 +4,6 @@ import com.example.crud.converter.Converter;
 import com.example.crud.client.Client;
 import com.example.crud.dto.ClientDto;
 import com.example.crud.repo.ClientRepository;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
@@ -17,7 +15,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.function.Function;
 
 @RestController
 public class Controller {
@@ -51,16 +48,17 @@ public class Controller {
         return converter.entityToDto((List<Client>) findall);
     }
 
-    @GetMapping("/data/{pageNumber}")
-    public Page<ClientDto> getClientPageOne(@PathVariable Integer pageNumber){
-        Pageable pageable = PageRequest.of((pageNumber-1),20);
-        Page<Client> clt = clientRepository.findAll(pageable);
-        return clt.map(new Function<Client, ClientDto>() {
-            @Override
-            public ClientDto apply(Client client) {
-                return new ModelMapper().map(client, ClientDto.class);
-            }
-        });
+    @GetMapping("/data")
+    public Page<ClientDto> getClientPageOne(Pageable pageable){
+//        Pageable pageable = PageRequest.of((pageNumber-1),20);
+//        Page<Client> clt = clientRepository.findAll(pageable);
+//        return clt.map(new Function<Client, ClientDto>() {
+//            @Override
+//            public ClientDto apply(Client client) {
+//                return new ModelMapper().map(client, ClientDto.class);
+//            }
+//        });
+        return clientRepository.findAll(pageable).map(client -> new ModelMapper().map(client, ClientDto.class));
     }
 
     @GetMapping("/dataName")
